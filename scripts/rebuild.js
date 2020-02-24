30d6528cc87cd2047e0167dfdad0f9cfaef37f80
Dec-F <dec_fan@icloud.com>
2020-02-24 18:59:04 +0800

    feat: build in sharp

--- scripts/rebuild.js
+++ scripts/rebuild.js
@@ -1,18 +1,14 @@
-const ELECTRON_VERSION = "6.1.7";
-
+const platform = require("os").platform;
+const fs = require("fs");
 const path = require("path");
-const rebuild = require("electron-rebuild").default;
-
-console.log("path:", path.join(__dirname, "../../../"));
-rebuild({
-  buildPath: path.join(__dirname, "../../../"),
-  force: true,
-  debug: true,
-  useCache: false,
-  electronVersion: ELECTRON_VERSION
-})
-  .then(() => console.info("Rebuild Successful"))
-  .catch(e => {
-    console.error("Building modules didn't work!");
-    console.error(e);
-  });
+const sourcePath = path.join(
+  __dirname,
+  `../external/sharp/${platform}/sharp.node`
+);
+const targetPath = path.join(
+  __dirname,
+  `../../../node_modules/sharp/build/Release/sharp.node`
+);
+fs.rename(sourcePath, targetPath, err => {
+  if (err) throw new Error("copy sharp error");
+});
